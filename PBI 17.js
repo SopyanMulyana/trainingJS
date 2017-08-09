88b25b3e96ae1dcd4c2960510eed1646fe29198b
Daniel Muliawan <danielmuliawann@gmail.com>
2017-08-09 18:03:00 +0800

    Implements Reusable JS Functions

---
+++ PBI 17.js
@@ -0,0 +1,12 @@
+// Implements Reusable JS Functions
+function ourReusableFunction() {
+  console.log("Hello, World");
+}
+
+ourReusableFunction();
+
+function reusableFunction() {
+  console.log("Hai World");
+}
+
+reusableFunction();
